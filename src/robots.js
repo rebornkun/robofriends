4b6b4b71e0c82cc2e6a54668e75f3d96b7b5cc88
Oluyoh okiemute <OLUYOHOKIEMUTE@GMAIL.COM>
2022-08-15 09:18:38 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+
+    {
+        id: 1,
+        name: 'Karina peretei',
+        username: 'beingX',
+        email: 'kairnnnii@kdkd.com'
+    },
+    {
+        id: 2,
+        name: 'Obinna udekku',
+        username: 'Bryann',
+        email: 'BByywuww@kdww.com'
+    },
+    {
+        id: 3,
+        name: 'Anthony Drake',
+        username: 'Anthoboy',
+        email: 'Anthoboy@kdkd.com'
+    },
+    {
+        id: 4,
+        name: 'Hitman Reborn',
+        username: 'Hitrun',
+        email: 'Hitrun@kdkd.com'
+    },
+    {
+        id: 5,
+        name: 'Don Efe',
+        username: 'Hitech',
+        email: 'Hitech@kdkd.com'
+    },
+    {
+        id: 6,
+        name: 'Hollndia yogo',
+        username: 'holygo',
+        email: 'holygo@kdkd.com'
+    },
+    {
+        id: 7,
+        name: 'Multi meter',
+        username: 'mmuuyy',
+        email: 'mmuuyy@hotmail.com'
+    },
+    {
+        id: 8,
+        name: 'Ben Ten',
+        username: 'youngman',
+        email: 'youngman@kdkd.com'
+    },
+    {
+        id: 9,
+        name: 'Anng Air',
+        username: 'Avatar',
+        email: 'Avatar@air.earth'
+    },
+    {
+        id: 10,
+        name: 'Son Goku',
+        username: 'ultra',
+        email: 'Ssaiyan22@kame.hameha'
+    },
+    
+    
+]
